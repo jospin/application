dc8ebe7d8cc69f0051f261c480e83a01e7186b2f
jospin <lucien.carbonare@gmail.com>
2015-01-26 18:17:23 -0200

    Montando o index para a chamada do Load

--- public/index.php
+++ public/index.php
@@ -1,4 +1,11 @@
 <?php
 
-require_once(dirname(__DIR__) . '/autload.php');
+/**
+* File is very important for skeleton of the application
+* I use redirect_url cause it call parameters witch may or may not exist.
+*/
+
+$redirect_url = explode('/',$_SERVER['REDIRECT_URL']);
+// Passa para a classe Load que ira tratar o Módulo/Controller/Action e os parâmetros
 echo '<pre>';
+print_r($_SERVER);
